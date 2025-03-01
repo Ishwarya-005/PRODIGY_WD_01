473966c03f679fa9e006b18196a50185ea0ce5ae
Ishwarya-005 <iedupuga@gitam.in>
2025-03-01 14:23:33 +0530

    Added about me section and made it scrollable

--- Task-1/index.js
+++ Task-1/index.js
@@ -1,2 +1,14 @@
 
+document.querySelector('a[href="#about"]').addEventListener('click', function (e) {
+    e.preventDefault(); 
+
+
+    document.body.style.overflow = 'auto';
+
+    const aboutSection = document.getElementById('about');
+    aboutSection.style.top = '0';
+
+
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+});
 
